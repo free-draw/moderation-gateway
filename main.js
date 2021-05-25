6b5abbea142230729130e3e183cc4925fb868b0b
Reselim <notreselim@gmail.com>
2021-05-25 06:04:17 -0400

    Only use dotenv in development

--- main.js
+++ main.js
@@ -3,15 +3,17 @@ require("make-promises-safe")
 const http = require("http")
 const path = require("path")
 const lodash = require("lodash")
-const dotenv = require("dotenv")
 const io = require("socket.io")
 const redis = require("redis")
 
 const config = require("./config.json")
 
-dotenv.config({
-	path: path.join(__dirname, ".env"),
-})
+if (process.env.NODE_ENV === "development") {
+	const dotenv = require("dotenv")
+	dotenv.config({
+		path: path.join(__dirname, ".env"),
+	})
+}
 
 const server = new http.Server()
 const socket = io(server)
